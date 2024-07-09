3b9464e6da0f34e88a049f37ad64177597944436
Marta Orwat <marta.aleksandra.orwat@cern.ch>
2024-07-09 15:23:05 +0200

    edits

--- aper_package/aperture_plot.py
+++ aper_package/aperture_plot.py
@@ -1,29 +1,40 @@
+import sys
+sys.path.append('/eos/home-i03/m/morwat/.local/lib/python3.9/site-packages/')
+
 import numpy as np
-import tfs
-import plotly.graph_objects as go
 import pandas as pd
-import xtrack as xt
+import tfs
 import yaml
+
+import xtrack as xt
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from aper_package.utils import match_indices
 
 class AperPlot:
     
     #TODO define all the arguments as optional or not
-    def __init__(self, 
-                 plane,
-                 beam,
+    def __init__(self,
+                 emitt = 3.5e-6,
+                 gamma = 7247.364689,
                  show_both_beams = True,
                  path1 = "/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/MADX/levelling.20/all_optics_B1.tfs", 
                  path2 = "/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/MADX/levelling.20/all_optics_B4.tfs",
                  line1 = '/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/xsuite/levelling.20_b1.json',
                  line2 = '/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/xsuite/levelling.20_b2.json',
                  machine_components_path = '/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/MADX_thick/injection/all_optics_B1.tfs',
-                 collimators_path = '/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/colldbs/levelling.20.yaml'
-                 remove_stuff = True,):
+                 collimators_path = '/eos/project-c/collimation-team/machine_configurations/LHC_run3/2023/colldbs/levelling.20.yaml'):
+        
+        self.emitt = emitt
+        self.gamma = gamma
         
         df_b1 = tfs.read(path1)
         df_b2 = tfs.read(path2)
+
+        self.machine_components_path = machine_components_path
+        self.collimators_path = collimators_path
         
         #get rid of undefined values
         indices_b1 = df_b1.index[(df_b1['APER_1'] < 1) & (df_b1['APER_1'] != 0) & (df_b1['APER_2'] < 1) & (df_b1['APER_2'] != 0)].tolist()
@@ -38,26 +49,15 @@ class AperPlot:
         aperx_b2 = df_b2["APER_1"][indices_b2].to_numpy()
         apery_b2 = df_b2["APER_2"][indices_b2].to_numpy()
 
-        #define normalised emittance
-        emitt=3.5e-6
-        #define gamma
-        gamma = 7247.364689 #TODO don't hardcode it
-
         # Load a line and build tracker
         line_b1 = xt.Line.from_json(line1)
         line_b2 = xt.Line.from_json(line2)
 
-        if remove_stuff:
-            line_b1.merge_consecutive_drifts()
-            line_b2.merge_consecutive_drifts()
-            line_b1.remove_markers()
-            line_b2.remove_markers()
-            line_b1.remove_inactive_multipoles()
-            line_b2.remove_inactive_multipoles()
-            line_b1.remove_zero_length_drifts()
-            line_b2.remove_zero_length_drifts()
-            line_b1.remove_redundant_apertures()
-            line_b2.remove_redundant_apertures()
+        print('Computing twiss for beam 1...')
+        tw_b1 = line_b1.twiss()
+        print('Computing twiss for beam 2...')
+        tw_b2 = line_b2.twiss(reverse=True)
+        print('Done computing twiss.')
 
         # Remove all the aperture
         tw_b1 = tw_b1.to_pandas()[~tw_b1.to_pandas().name.str.contains('aper')]
@@ -69,6 +69,8 @@ class AperPlot:
         tws_b2 = tw_b2.s.to_numpy()[-1]-tw_b2.s.to_numpy()
         
         # TODO maybe later shorten this:
+
+        print('Defining variables...')
 
         # Match the lengths of data for both s_b1 and tws_b1 to facilitate calculations 
         # of the distance of the beam from the aperture later
@@ -82,12 +84,12 @@ class AperPlot:
             # Horizontal data for beam 1
             self.x_b1 = tw_b1.x.to_numpy()
             self.betx_b1 = tw_b1.betx.to_numpy()
-            self.sigmax_b1 = np.sqrt(self.betx_b1*emitt/gamma)
+            self.sigmax_b1 = np.sqrt(self.betx_b1*self.emitt/self.gamma)
 
             # Vertical data for beam 1
             self.y_b1 = tw_b1.y.to_numpy()
             self.bety_b1 = tw_b1.bety.to_numpy()
-            self.sigmay_b1 = np.sqrt(self.bety_b1*emitt/gamma)   
+            self.sigmay_b1 = np.sqrt(self.bety_b1*self.emitt/self.gamma)   
             
             # Aperture data shortened to match the length of twiss
             self.aperx_b1 = aperx_b1[indices]
@@ -103,55 +105,55 @@ class AperPlot:
             # Horizontal data for beam 1
             self.x_b1 = tw_b1.x.to_numpy()[indices]
             self.betx_b1 = tw_b1.betx.to_numpy()[indices]
-            self.sigmax_b1 = np.sqrt(self.betx_b1*emitt/gamma)
+            self.sigmax_b1 = np.sqrt(self.betx_b1*self.emitt/self.gamma)
 
             # Vertical data for beam 1
             self.y_b1 = tw_b1.y.to_numpy()[indices]
             self.bety_b1 = tw_b1.bety.to_numpy()[indices]
-            self.sigmay_b1 = np.sqrt(self.bety_b1*emitt/gamma)   
+            self.sigmay_b1 = np.sqrt(self.bety_b1*self.emitt/self.gamma)   
             
             # Aperture data shortened to match the length of twiss
             self.aperx_b1 = aperx_b1
             self.apery_b1 = apery_b1
        
         # Match the length of s_b2 and tws_b2
-        if len(s_b1) > len(tws_b1): 
+        if len(s_b2) > len(tws_b2): 
 
-            indices = match_indices(tws_b1, s_b1)
+            indices = match_indices(tws_b2, s_b2)
 
-            self.s_b1 = tws_b1
-            self.name = tw_b1.name.to_numpy()
+            self.s_b2 = tws_b2
+            self.name = tw_b2.name.to_numpy()
             
             # Horizontal data for beam 1
-            self.x_b1 = tw_b1.x.to_numpy()
-            self.betx_b1 = tw_b1.betx.to_numpy()
-            self.sigmax_b1 = np.sqrt(self.betx_b1*emitt/gamma)
+            self.x_b2 = -tw_b2.x.to_numpy()
+            self.betx_b2 = tw_b2.betx.to_numpy()
+            self.sigmax_b2 = np.sqrt(self.betx_b2*self.emitt/self.gamma)
 
             # Vertical data for beam 1
-            self.y_b1 = tw_b1.y.to_numpy()
-            self.bety_b1 = tw_b1.bety.to_numpy()
-            self.sigmay_b1 = np.sqrt(self.bety_b1*emitt/gamma)   
+            self.y_b2 = -tw_b2.y.to_numpy()
+            self.bety_b2 = tw_b2.bety.to_numpy()
+            self.sigmay_b2 = np.sqrt(self.bety_b2*self.emitt/self.gamma)   
             
             # Aperture data shortened to match the length of twiss
-            self.aperx_b1 = aperx_b1[indices]
-            self.apery_b1 = apery_b1[indices]
+            self.aperx_b2 = aperx_b2[indices]
+            self.apery_b2 = apery_b2[indices]
              
         else:
 
-            indices = match_indices(s_b1, tws_b1)
+            indices = match_indices(s_b2, tws_b2)
 
             self.s_b2 = s_b2
             self.name = tw_b2.name.to_numpy()[indices]
             
             # Horizontal data for beam 1
-            self.x_b2 = tw_b2.x.to_numpy()[indices]
+            self.x_b2 = -tw_b2.x.to_numpy()[indices]## Find locations of IPs
             self.betx_b2 = tw_b2.betx.to_numpy()[indices]
-            self.sigmax_b2 = np.sqrt(self.betx_b2*emitt/gamma)
+            self.sigmax_b2 = np.sqrt(self.betx_b2*self.emitt/self.gamma)
 
             # Vertical data for beam 1
-            self.y_b2 = tw_b2.y.to_numpy()[indices]
+            self.y_b2 = -tw_b2.y.to_numpy()[indices]
             self.bety_b2 = tw_b2.bety.to_numpy()[indices]
-            self.sigmay_b2 = np.sqrt(self.bety_b2*emitt/gamma)   
+            self.sigmay_b2 = np.sqrt(self.bety_b2*self.emitt/self.gamma)   
             
             # Aperture data shortened to match the length of twiss
             self.aperx_b2 = aperx_b2
@@ -159,10 +161,16 @@ class AperPlot:
 
         self.nominalx_b1 = self.x_b1
         self.nominalx_b2 = self.x_b2
+        
         self.nominaly_b1 = self.y_b1
         self.nominaly_b2 = self.y_b2
 
         self.envelope(0)
+
+        ## Find locations of IPs
+        self.ip_names = np.array(['ip1', 'ip2', 'ip5', 'ip8'])
+        self.ip_b1 = tw_b1.loc[tw_b1['name'].isin(self.ip_names)].s.to_numpy()
+        self.ip_b2 = tw_b2.loc[tw_b2['name'].isin(self.ip_names)].s.to_numpy()
         
         del tw_b1, tw_b2        
         
@@ -183,12 +191,14 @@ class AperPlot:
 
     def _rescale_factor(self, angle, ip, plane, lim):
         
+        #TODO add code for IP1
+
         # Find iposition of ip
-        ip_s = self.s[np.where(self.name == ip)[0][0]]
+        ip_s = self.ip_b1[np.where(self.ip_names == ip)[0][0]]
 
         #use only the data up to 20 m away from the ip
-        ind = np.where(((self.s - ip_s) >= 0) & ((self.s - ip_s) < lim))
-        s = self.s[ind]
+        ind = np.where(((self.s_b1 - ip_s) >= 0) & ((self.s_b1 - ip_s) < lim))
+        s = self.s_b1[ind]
         if plane == 'h':
             x = self.x_b1[ind]
         elif plane == 'v':
@@ -216,14 +226,15 @@ class AperPlot:
 
         self.envelope(self.n)        
 
-    def _add_collimators(self, path, fig):
+    def _add_collimators(self, fig, row, column):
 
         #TODO
         pass
-    
-    def _load_machine_components(self, path, objects, colors):
 
-        df = tfs.read(path)
+
+    def _load_machine_components(self):
+
+        df = tfs.read(self.machine_components_path)
         
         s = df['S'].to_numpy()
         keyword = df['KEYWORD'].to_numpy()
@@ -234,18 +245,19 @@ class AperPlot:
         return s, keyword, l, k1l, name
         
 
-    def _add_machine_components(self, path, fig):
+    def _add_machine_components(self, fig, row, column):
         
-        s, keyword, l, k1l, name = self._load_machine_components(path, objects, colors)
+        s, keyword, l, k1l, name = self._load_machine_components()
 
-        #TODO
+        objects = ["SBEND", "COLLIMATOR", "SEXTUPOLE", "RBEND", "QUADRUPOLE"]
+        colors = ['lightblue', 'black', 'hotpink', 'green', 'red']
 
-        for n, j in enumerate(self.objects):
+        for n, j in enumerate(objects):
             
-            ob_s = self.s[np.where(self.keyword==j)[0]]
-            ob_l = self.l[np.where(self.keyword==j)[0]]
-            ob_name = self.name[np.where(self.keyword==j)[0]]
-            ob_k1l = np.sign(self.k1l[np.where(self.keyword=="QUADRUPOLE")[0]]).astype(int)
+            ob_s = s[np.where(keyword==j)[0]]
+            ob_l = l[np.where(keyword==j)[0]]
+            ob_name = name[np.where(keyword==j)[0]]
+            ob_k1l = np.sign(k1l[np.where(keyword=="QUADRUPOLE")[0]]).astype(int)
             
             for i in range(len(ob_s)):
 
@@ -261,7 +273,8 @@ class AperPlot:
                     y1=0.5
 
                 fig.add_trace(go.Scatter(x=[x0, x0, x1, x1], y=[y0, y1, y1, y0], fill="toself", mode='lines',
-                             fillcolor=self.colors[n], line=dict(color=self.colors[n]), name=ob_name[i]), row=row, col=column)  
+                             fillcolor=colors[n], line=dict(color=colors[n]), name=ob_name[i]), row=row, col=column)  
+
 
     def distance_from_nominal(self):
 
@@ -272,75 +285,105 @@ class AperPlot:
 
         #TODO
         pass
-            
-    def plot(self, 
-             plane, 
-             fig, 
-             beam,
-             show_both_beams = True,
-             show_collimators = True):
+
+
+    def show(self, 
+             plane,
+             beam = 1,
+             show_both_beams = True):
+        
+        # Create figure
+        fig = make_subplots(rows=2, cols=1, row_heights=[0.2, 0.8], shared_xaxes=True)
         
         if plane == 'h':
             
-            top_aper_b1 = go.Scatter(x=self.s, y=self.x_b1, mode='lines', line=dict(color='gray'), name='Aperture')
-            bottom_aper_b1 = go.Scatter(x=self.s, y=-self.x_b1, mode='lines', line=dict(color='gray'), name='Aperture')     
-            top_aper_b2 = go.Scatter(x=self.s, y=self.x_b2, mode='lines', line=dict(color='gray'), name='Aperture')
-            bottom_aper_b2 = go.Scatter(x=self.s, y=-self.x_b2, mode='lines', line=dict(color='gray'), name='Aperture') 
-            
-        if plane == 'v':
-            
-            top_aper_b1 = go.Scatter(x=self.s, y=self.y_b1, mode='lines', line=dict(color='gray'), name='Aperture')
-            bottom_aper_b1 = go.Scatter(x=self.s, y=-self.y_b1, mode='lines', line=dict(color='gray'), name='Aperture')
-            top_aper_b2 = go.Scatter(x=self.s, y=self.y_b2, mode='lines', line=dict(color='gray'), name='Aperture')
-            bottom_aper_b2 = go.Scatter(x=self.s, y=-self.y_b2, mode='lines', line=dict(color='gray'), name='Aperture') 
+            top_aper_b1 = go.Scatter(x=self.s_b1, y=self.aperx_b1, mode='lines', line=dict(color='gray'), name='Aperture')
+            bottom_aper_b1 = go.Scatter(x=self.s_b1, y=-self.aperx_b1, mode='lines', line=dict(color='gray'), name='Aperture')     
+            top_aper_b2 = go.Scatter(x=self.s_b2, y=self.aperx_b2, mode='lines', line=dict(color='gray'), name='Aperture')
+            bottom_aper_b2 = go.Scatter(x=self.s_b2, y=-self.aperx_b2, mode='lines', line=dict(color='gray'), name='Aperture') 
 
-        if plane == 'h':
-            
-            b1 = go.Scatter(x=self.s, y=self.x_b1, mode='lines', line=dict(color='blue'), name='Beam 1')
-            b2 = go.Scatter(x=self.s, y=self.x_b2, mode='lines', line=dict(color='red'), name='Beam 2')
-            nom_b1 = go.Scatter(x=self.s, y=self.nominalx_b1, mode='lines', line=dict(color='blue', dash='dash'), name='Beam 1')
-            nom_b2 = go.Scatter(x=self.s, y=self.nominalx_b2, mode='lines', line=dict(color='red', dash='dash'), name='Beam 2')
+            b1 = go.Scatter(x=self.s_b1, y=self.x_b1, mode='lines', line=dict(color='blue'), name='Beam 1')
+            b2 = go.Scatter(x=self.s_b2, y=self.x_b2, mode='lines', line=dict(color='red'), name='Beam 2')
+            nom_b1 = go.Scatter(x=self.s_b1, y=self.nominalx_b1, mode='lines', line=dict(color='blue', dash='dash'), name='Beam 1')
+            nom_b2 = go.Scatter(x=self.s_b2, y=self.nominalx_b2, mode='lines', line=dict(color='red', dash='dash'), name='Beam 2')
             
             up_b1 = self.xup_b1
             down_b1 = self.xdown_b1
             up_b2 = self.xup_b2
             down_b2 = self.xdown_b2
             
-        if plane == 'v':
+            fig.update_yaxes(range=[-0.05, 0.05], title_text="x [m]", row=2, col=1)
             
-            b1 = go.Scatter(x=self.s, y=self.y_b1, mode='lines', line=dict(color='blue'), name='Beam 1')
-            b2 = go.Scatter(x=self.s, y=self.y_b2, mode='lines', line=dict(color='red'),name='Beam 2')
-            nom_b1 = go.Scatter(x=self.s, y=self.nominaly_b1, mode='lines', line=dict(color='blue', dash='dash'), name='Beam 1')
-            nom_b2 = go.Scatter(x=self.s, y=self.nominaly_b2, mode='lines', line=dict(color='red', dash='dash'), name='Beam 2')
+        elif plane == 'v':
+            
+            top_aper_b1 = go.Scatter(x=self.s_b1, y=self.apery_b1, mode='lines', line=dict(color='gray'), name='Aperture')
+            bottom_aper_b1 = go.Scatter(x=self.s_b1, y=-self.apery_b1, mode='lines', line=dict(color='gray'), name='Aperture')
+            top_aper_b2 = go.Scatter(x=self.s_b2, y=self.apery_b2, mode='lines', line=dict(color='gray'), name='Aperture')
+            bottom_aper_b2 = go.Scatter(x=self.s_b2, y=-self.apery_b2, mode='lines', line=dict(color='gray'), name='Aperture') 
+            
+            b1 = go.Scatter(x=self.s_b1, y=self.y_b1, mode='lines', line=dict(color='blue'), name='Beam 1')
+            b2 = go.Scatter(x=self.s_b2, y=self.y_b2, mode='lines', line=dict(color='red'),name='Beam 2')
+            nom_b1 = go.Scatter(x=self.s_b1, y=self.nominaly_b1, mode='lines', line=dict(color='blue', dash='dash'), name='Beam 1')
+            nom_b2 = go.Scatter(x=self.s_b2, y=self.nominaly_b2, mode='lines', line=dict(color='red', dash='dash'), name='Beam 2')
             
             up_b1 = self.yup_b1
             down_b1 = self.ydown_b1
             up_b2 = self.yup_b2
             down_b2 = self.ydown_b2
 
+            fig.update_yaxes(range=[-0.05, 0.05], title_text="y [m]", row=2, col=1)
 
-        upper_b1 = go.Scatter(x=self.s, y=up_b1, mode='lines', name='Upper envelope beam 1', fill=None, line=dict(color='rgba(0,0,255,0)'))
-        lower_b1 = go.Scatter(x=self.s, y=down_b1, mode='lines', name='Lower envelope beam 1', line=dict(color='rgba(0,0,255,0)'), 
+        else: print('Incorrect plane')
+
+
+        upper_b1 = go.Scatter(x=self.s_b1, y=up_b1, mode='lines', name='Upper envelope beam 1', fill=None, line=dict(color='rgba(0,0,255,0)'))
+        lower_b1 = go.Scatter(x=self.s_b1, y=down_b1, mode='lines', name='Lower envelope beam 1', line=dict(color='rgba(0,0,255,0)'), 
                           fill='tonexty', fillcolor='rgba(0,0,255,0.1)')
 
-        upper_b2 = go.Scatter(x=self.s, y=up_b2, mode='lines', name='Upper envelope beam 2', fill=None, line=dict(color='rgba(255,0,0,0)'))
-        lower_b2 = go.Scatter(x=self.s, y=down_b2, mode='lines', name='Lower envelope beam 2', line=dict(color='rgba(255,0,0,0)'), 
+        upper_b2 = go.Scatter(x=self.s_b2, y=up_b2, mode='lines', name='Upper envelope beam 2', fill=None, line=dict(color='rgba(255,0,0,0)'))
+        lower_b2 = go.Scatter(x=self.s_b2, y=down_b2, mode='lines', name='Lower envelope beam 2', line=dict(color='rgba(255,0,0,0)'), 
                            fill='tonexty', fillcolor='rgba(255,0,0,0.1)')
 
-        traces_b1 = [b1, nom_b1, upper_b1, lower_b1, top_aper_b1, bottom_aper_b1]
-        traces_b2 = [b2, nom_b2, upper_b2, lower_b2, top_aper_b2, bottom_aper_b1]
+        traces_b1 = [b1, nom_b1, upper_b1, lower_b1]
+        traces_b2 = [b2, nom_b2, upper_b2, lower_b2]
         
-        if beam1:
+        if beam == 1:
             for i in traces_b1:
-                fig.add_trace(i, row=row, col=column) 
+                fig.add_trace(i, row=2, col=1) 
+
+            fig.add_trace(top_aper_b1, row=2, col=1)
+            fig.add_trace(bottom_aper_b1, row=2, col=1)
+
+            if show_both_beams:
+                for i in traces_b2:
+                    fig.add_trace(i, row=2, col=1) 
+                
         
-        if beam2:
+        if beam == 2:
             for i in traces_b2:
-                fig.add_trace(i, row=row, col=column) 
+                fig.add_trace(i, row=2, col=1) 
 
-        if show_collimators:
+            fig.add_trace(top_aper_b2, row=2, col=1)
+            fig.add_trace(bottom_aper_b2, row=2, col=1)
 
-            _add_collimators(path, fig)
+            if show_both_beams:
+                for i in traces_b1:
+                    fig.add_trace(i, row=2, col=1) 
+
+
+        self._add_machine_components(fig=fig, row=1, column=1)
+
+        # Set layout
+        fig.update_layout(height=800, width=800, plot_bgcolor='white', showlegend=False)
+
+        # Change the axes limits and labels
+        fig.update_yaxes(range=[-1, 1], row=1, col=1, showticklabels=False, showline=False)
+
+        fig.update_xaxes(title_text="s [m]", row=2, col=1)
+        fig.update_xaxes(row=1, col=1, showticklabels=False, showline=False)
+
+        fig.show()
+        
 
                 
 
