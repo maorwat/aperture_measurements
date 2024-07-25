7e44ca641d13b9db09b9a55a71941eac61867fa1
Marta Orwat <marta.aleksandra.orwat@cern.ch>
2024-07-25 09:13:58 +0200

    Finished and tested plotting.py

--- aper_package/plotting.py
+++ aper_package/plotting.py
@@ -3,29 +3,115 @@ sys.path.append('/eos/home-i03/m/morwat/.local/lib/python3.9/site-packages/')
 
 import numpy as np
 import pandas as pd
-import tfs
-import yaml
 
 import warnings
 warnings.filterwarnings('ignore')
 
-import xtrack as xt
-
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-from aper_package.utils import shift_by
-from aper_package.utils import select_file
+def plot(data, plane, add_VELO=True):
 
-def plot(data, plane):
-    
-    fig = make_subplots(rows=2, cols=1, row_heights=[0.2, 0.8], shared_xaxes=True)
+    # For buttons
+    visibility_b1 = np.array([], dtype=bool)
+    visibility_b2 = np.array([], dtype=bool)
 
     if hasattr(data, 'elements'):
-        visibility_b1, elements = machine_components(data)
-        for i in data.elements:
+
+        fig = make_subplots(rows=2, cols=1, row_heights=[0.2, 0.8], shared_xaxes=True)
+        fig.update_yaxes(range=[-1, 1], showticklabels=False, showline=False, row=1, col=1)
+        fig.update_xaxes(showticklabels=False, showline=False, row=1, col=1)
+        el_vis, elements = machine_components(data)
+
+        for i in elements:
             fig.add_trace(i, row=1, col=1)
+        # Row and col for other traces
+        row, col = 2, 1
+
+        visibility_b1 = np.append(visibility_b1, el_vis)
+        visibility_b2 = np.append(visibility_b2, el_vis)
+
+    else:
+        fig = make_subplots(rows=1, cols=1)
+        # Row and col for other traces
+        row, col = 1, 1
+
+    # If there is aperture data
+    if hasattr(data, 'aper_b1'):
+        aper_vis, apertures = aperture(data, plane)
+        for i in apertures:
+            fig.add_trace(i, row=row, col=col)
+
+        visibility_b1 = np.append(visibility_b1, aper_vis)
+        visibility_b2 = np.append(visibility_b2, np.logical_not(aper_vis))
+
+    # If there are collimators 
+    if hasattr(data, 'colx_b1'):
+        col_vis, collimator = collimators(data, plane)
+        for i in collimator:
+            fig.add_trace(i, row=row, col=col)
+
+        visibility_b1 = np.append(visibility_b1, col_vis)
+        visibility_b2 = np.append(visibility_b2, np.logical_not(col_vis))
+
+    # Add beam positions
+    beam_vis, beams = beam_positions(data, plane)
+    for i in beams:
+        fig.add_trace(i, row=row, col=col)
+
+    visibility_b1 = np.append(visibility_b1, beam_vis)
+    visibility_b2 = np.append(visibility_b2, beam_vis)
+
+    # Add nominal beam positions
+    nom_beam_vis, nom_beams = nominal_beam_positions(data, plane)
+    for i in nom_beams:
+        fig.add_trace(i, row=row, col=col)
+
+    visibility_b1 = np.append(visibility_b1, nom_beam_vis)
+    visibility_b2 = np.append(visibility_b2, nom_beam_vis)
+
+    # Add envelopes
+    env_vis, envelope = envelopes(data, plane)
+    for i in envelope:
+        fig.add_trace(i, row=row, col=col)
+    
+    visibility_b1 = np.append(visibility_b1, env_vis)
+    visibility_b2 = np.append(visibility_b2, env_vis)
+
+    if add_VELO: add_velo(data, fig=fig, row=row, col=col)
+
+    # Define buttons to toggle aperture and collimator data
+    buttons = [
+            dict(
+                label="Beam 1",
+                method="update",
+                args=[{"visible": visibility_b1}]),
+            dict(
+                label="Beam 2",
+                method="update",
+                args=[{"visible": visibility_b2}])]
+    
+    # Set initial visibility for the traces
+    for i, trace in enumerate(fig.data):
+        trace.visible = visibility_b1[i]
+
+    # Set layout
+    fig.update_layout(height=800, width=800, plot_bgcolor='white', showlegend=False)
+
+    # Change the axes limits and labels
+    fig.update_xaxes(title_text="s [m]", row=row, col=col)
+    fig.update_yaxes(range = [-0.05, 0.05], row=row, col=col)
+
+    # Add the buttons to the layout
+    fig.update_layout(updatemenus=[{"buttons": buttons, "showactive": True, 'active': 0, 'pad': {"r": 10, "t": 10},
+                                    'xanchor': "left", 'yanchor': 'top', 'x': 0.15, 'y': 1.1}])
         
+    # Add annotation
+    fig.update_layout(annotations=[dict(text="Aperture data:", showarrow=False, 
+                    x=0, y=1.075, xref="paper", yref='paper', align="left")])
+        
+    fig.show()
+
 def machine_components(data):
 
     # Specify the elements to plot and corresponding colors
@@ -148,8 +234,8 @@ def beam_positions(data, plane):
 
 def nominal_beam_positions(data, plane):
 
-    if plane == 'h': y_b1, y_b2 = data.nom_b1.x, data.nom_b1.x
-    elif plane == 'v': y_b1, y_b2 = data.nom_b2.y, data.nom_b2.y
+    if plane == 'h': y_b1, y_b2 = data.nom_b1.x, data.nom_b2.x
+    elif plane == 'v': y_b1, y_b2 = data.nom_b1.y, data.nom_b2.y
 
     b1 = go.Scatter(x=data.nom_b1.s, y=y_b1, mode='lines', line=dict(color='blue', dash='dash'), text = data.nom_b1.name, name='Beam 1')
     b2 = go.Scatter(x=data.nom_b2.s, y=y_b2, mode='lines', line=dict(color='red', dash='dash'), text = data.nom_b2.name, name='Beam 2')
@@ -179,8 +265,16 @@ def aperture(data, plane):
 
     return visibility, traces
 
-def add_velo(data):
+def add_velo(data, fig, row, col):
 
     ip8 = data.tw_b1.loc[data.tw_b1['name'] == 'ip8', 's'].values[0]
 
+    fig.add_shape(type="rect",
+                x0=(ip8-0.2), x1=(ip8+0.8),
+                y0=-0.05, y1=0.05,
+                xref='x',
+                yref='y',
+                line=dict(
+                    color="Orange",
+                    width=1), row=row, col=col)
 
