e28b1d5a3b1e0c63e9ed6c19ef8f6bd26cd4f965
Marta Orwat <marta.aleksandra.orwat@cern.ch>
2024-08-09 17:05:19 +0200

    some tidying up

--- aper_package/plotting.py
+++ aper_package/plotting.py
@@ -9,26 +9,29 @@ from plotly.subplots import make_subplots
 from ipywidgets import widgets, VBox, HBox, Button, Layout, FloatText
 from IPython.display import display
 
-def plot(data, plane, BPMs = None, collimators = None):
+def plot(data, plane, BPM_data = None, collimator_data = None, width=1000, height=600, additional_traces=None):
 
     global knob_dropdown, add_button, remove_button, apply_button, graph_container, knob_box
     global values, selected_knobs, knob_widgets
-    global aper_data, aper_plane, beam_position_data, col_data
+    global global_data, global_plane, global_BPM_data, global_collimator_data
+    global global_width, global_height, global_additional_traces
 
-    aper_data = data          #TODO: make the names make sense
-    aper_plane = plane
-    beam_position_data = BPMs
-    col_data = collimators
+    # Set the arguments as global
+    global_data = data
+    global_plane = plane
+    global_BPM_data = BPM_data
+    global_collimator_data = collimator_data
+    global_width = width
+    global_height = height
+    global_additional_traces = additional_traces
 
-    fig, visibility_b1, visibility_b2, row, col = create_figure(aper_data, aper_plane, beam_position_data, col_data)
-
-    # Dictionary to store multipliers for each option
-    values = {}
+    # Dictionaries and list to store selected knobs and corresponding widgets and current widget values
     selected_knobs = []
     knob_widgets = {}
+    values = {}
 
     # Create a dropdown to select a knob
-    knob_dropdown = add_knob_dropdown()
+    knob_dropdown = create_knob_dropdown()
 
     # Button to add selection
     add_button = Button(description="Add", button_style='success')
@@ -37,7 +40,7 @@ def plot(data, plane, BPMs = None, collimators = None):
     # Button to apply selection and update graph
     apply_button = Button(description="Apply", button_style='primary')
 
-    # Create an empty VBox container for the graph and multiplier widgets
+    # Create an empty VBox container for the graph and widgets
     graph_container = VBox(layout=Layout(
         justify_content='center',
         align_items='center',
@@ -46,6 +49,7 @@ def plot(data, plane, BPMs = None, collimators = None):
         border='solid 2px #eee'
     ))
     
+    # Create a box to store selected knobs
     knob_box = VBox(layout=Layout(
         justify_content='center',
         align_items='center',
@@ -54,7 +58,7 @@ def plot(data, plane, BPMs = None, collimators = None):
         border='solid 2px #eee'
     ))
 
-    # Arrange widgets in a layout
+    # Arrange knob dropdown and buttons in a horizontal layout
     controls = HBox(
         [knob_dropdown, add_button, remove_button, apply_button],
         layout=Layout(
@@ -79,7 +83,7 @@ def plot(data, plane, BPMs = None, collimators = None):
         )
     )
 
-    # Display the widgets
+    # Display the widgets and the graph
     display(everything)
 
     # Set up event handlers
@@ -87,164 +91,209 @@ def plot(data, plane, BPMs = None, collimators = None):
     remove_button.on_click(on_remove_button_clicked)
     apply_button.on_click(on_apply_button_clicked)
 
-    update_graph(aper_data, aper_plane, beam_position_data, col_data)
+    # Plot all traces
+    update_graph(global_data, global_plane, global_BPM_data, global_collimator_data, global_additional_traces)
 
-# Function to handle adding a selected option
 def on_add_button_clicked(b):
+
+    """
+    Function to handle adding a knob
+    """
+    # Knob selected in the dropdown menu
     knob = knob_dropdown.value
+    # If the knob is not already in the selected list, add it
     if knob and knob not in selected_knobs:
         selected_knobs.append(knob)
-        values[knob] = 1.0  # Initialize multiplier for new option
+        values[knob] = 1.0  # Initialize knob for new value
 
-        # Create a new FloatText widget for the selected option
+        # Create a new FloatText widget for the selected knob
         knob_widget = FloatText(
             value=1.0,
             description=f'{knob}',
             disabled=False,
-            layout=Layout(width='270px')  # Adjust width as needed
+            layout=Layout(width='270px')
         )
+        # Add the widget to the knob widgets list
         knob_widgets[knob] = knob_widget
 
-        # Update selected options label and display multiplier widgets
+        # Update selected knobs and display value
         update_knob_box()
 
-# Function to handle removing a selected option
 def on_remove_button_clicked(b):
+    """
+    Function to handle removing a knob
+    """
+    # Knob selected in the dropdown menu
     knob = knob_dropdown.value
+    # If the knob is in the selected list, remove it
     if knob in selected_knobs:
         selected_knobs.remove(knob)
-        del values[knob]  # Remove multiplier for the option
+        del values[knob]  # Remove the value of the knob
         if knob in knob_widgets:
-            del knob_widgets[knob]  # Remove the multiplier widget
+            del knob_widgets[knob]  # Remove the widget
         
-        # Update selected options label and display multiplier widgets
+        # Update selected knobs and display value
         update_knob_box()
         
-# Function to update the knob_box layout
 def update_knob_box():
+    """
+    Function to update the knob_box layout
+    """
     # Group the widgets into sets of three per row
     rows = []
     for i in range(0, len(selected_knobs), 3):
         row = HBox([knob_widgets[knob] for knob in selected_knobs[i:i+3]],
-                   layout=Layout(align_items='flex-start'))  # Align to left
+                   layout=Layout(align_items='flex-start'))
         rows.append(row)
 
     # Update the knob_box with the new rows
     knob_box.children = rows
 
-# Function to apply changes and update the graph
 def on_apply_button_clicked(b):
-    # Update multipliers dictionary based on current values in multiplier widgets
+    """
+    Function to apply changes and update the graph
+    """
+    # Update knobs dictionary based on current values in the knob widgets
     for knob, widget in knob_widgets.items():
-        aper_data.change_knob(knob, widget.value)
+        global_data.change_knob(knob, widget.value)
     
-    aper_data.twiss()
-        
-    update_graph(aper_data, aper_plane, beam_position_data, col_data)
+    # Re-twiss
+    global_data.twiss()
+    
+    # Update the figure
+    update_graph(global_data, global_plane, global_BPM_data, global_collimator_data, global_additional_traces)
 
-# Function to update the graph
-def update_graph(data, plane, BPMs, collimators):
-    fig, visibility_b1, visibility_b2, row, col = create_figure(data, plane, BPMs, collimators)
+def update_graph(data, plane, BPM_data, collimator_data, additional_traces):
+    """
+    Function to update the graph
+    """
+    # Create figure
+    fig, visibility_b1, visibility_b2, row, col = create_figure(data, plane, BPM_data, collimator_data, additional_traces)
     # Update figure layout
     update_layout(fig, plane, row, col, visibility_b1, visibility_b2)
-        
+    
+    # Change to a widget
     fig_widget = go.FigureWidget(fig)
-
+    # Put the figure in the graph container
     graph_container.children = [fig_widget]
         
-def create_figure(data, plane, BPMs, collimators):
+def create_figure(data, plane, BPM_data, collimator_data, additional_traces):
 
-    # For buttons
+    # These correspond to swapping between visibilities of aperture/collimators for beam 1 and 2
     visibility_b1 = np.array([], dtype=bool)
     visibility_b2 = np.array([], dtype=bool)
 
+    # If thick machine elements are loaded
     if hasattr(data, 'elements'):
 
+        # Create 2 subplots: for elements and the plot
         fig = make_subplots(rows=2, cols=1, row_heights=[0.2, 0.8], shared_xaxes=True)
+        # Update layout of the upper plot (machine components plot)
         fig.update_yaxes(range=[-1, 1], showticklabels=False, showline=False, row=1, col=1)
         fig.update_xaxes(showticklabels=False, showline=False, row=1, col=1)
-        el_vis, elements = machine_components(data)
+        elements_visibility, elements = plot_machine_components(data)
 
         for i in elements:
             fig.add_trace(i, row=1, col=1)
+
         # Row and col for other traces
         row, col = 2, 1
 
-        visibility_b1 = np.append(visibility_b1, el_vis)
-        visibility_b2 = np.append(visibility_b2, el_vis)
+        # Always show machine components
+        visibility_b1 = np.append(visibility_b1, elements_visibility)
+        visibility_b2 = np.append(visibility_b2, elements_visibility)
 
+    # If thick machine elements are not loaded
     else:
+        # Create only one plot
         fig = make_subplots(rows=1, cols=1)
         # Row and col for other traces
         row, col = 1, 1
 
+    # If any additional traces were given as an argument
+    if additional_traces:
+        for i in additional_traces:
+            fig.add_trace(i, row=row, col=col)
+            visibility_b1 = np.append(visibility_b1, True)
+            visibility_b2 = np.append(visibility_b2, True)
+
     # If there is aperture data
     if hasattr(data, 'aper_b1'):
-        aper_vis, apertures = aperture(data, plane)
+        aper_visibility, apertures = plot_aperture(data, plane)
         for i in apertures:
             fig.add_trace(i, row=row, col=col)
 
-        visibility_b1 = np.append(visibility_b1, aper_vis)
-        visibility_b2 = np.append(visibility_b2, np.logical_not(aper_vis))
+        # Show only aperture for one beam
+        visibility_b1 = np.append(visibility_b1, aper_visibility)
+        visibility_b2 = np.append(visibility_b2, np.logical_not(aper_visibility))
 
-    # If there are collimators 
+    # If there are collimators loaded from yaml file
     if hasattr(data, 'colx_b1'):
-        col_vis, collimator = collimators_from_yaml(data, plane)
+        collimator_visibility, collimator = plot_collimators_from_yaml(data, plane)
         for i in collimator:
             fig.add_trace(i, row=row, col=col)
 
-        visibility_b1 = np.append(visibility_b1, col_vis)
-        visibility_b2 = np.append(visibility_b2, np.logical_not(col_vis))
+        # Show only collimators for one beam
+        visibility_b1 = np.append(visibility_b1, collimator_visibility)
+        visibility_b2 = np.append(visibility_b2, np.logical_not(collimator_visibility))
 
-    if collimators:
-        col_vis, collimator = collimators_from_timber(collimators, data, plane)
+    # If collimators were loaded from timber
+    if collimator_data:
+        collimator_visibility, collimator = plot_collimators_from_timber(collimator_data, data, plane)
         for i in collimator:
             fig.add_trace(i, row=row, col=col)
 
-        visibility_b1 = np.append(visibility_b1, col_vis)
-        visibility_b2 = np.append(visibility_b2, np.logical_not(col_vis))
+        # Show only collimators for one beam
+        visibility_b1 = np.append(visibility_b1, collimator_visibility)
+        visibility_b2 = np.append(visibility_b2, np.logical_not(collimator_visibility))
 
-    # Add beam positions
-    beam_vis, beams = beam_positions(data, plane)
+    # Add beam positions from twiss data
+    beam_visibility, beams = plot_beam_positions(data, plane)
     for i in beams:
         fig.add_trace(i, row=row, col=col)
 
-    visibility_b1 = np.append(visibility_b1, beam_vis)
-    visibility_b2 = np.append(visibility_b2, beam_vis)
+    # Always show both beams
+    visibility_b1 = np.append(visibility_b1, beam_visibility)
+    visibility_b2 = np.append(visibility_b2, beam_visibility)
 
     # Add nominal beam positions
-    nom_beam_vis, nom_beams = nominal_beam_positions(data, plane)
-    for i in nom_beams:
+    nominal_beam_visibility, nominal_beams = plot_nominal_beam_positions(data, plane)
+    for i in nominal_beams:
         fig.add_trace(i, row=row, col=col)
 
-    visibility_b1 = np.append(visibility_b1, nom_beam_vis)
-    visibility_b2 = np.append(visibility_b2, nom_beam_vis)
+    # Always show both beams
+    visibility_b1 = np.append(visibility_b1, nominal_beam_visibility)
+    visibility_b2 = np.append(visibility_b2, nominal_beam_visibility)
 
     # Add envelopes
-    env_vis, envelope = envelopes(data, plane)
+    envelope_visibility, envelope = plot_envelopes(data, plane)
     for i in envelope:
         fig.add_trace(i, row=row, col=col)
     
-    visibility_b1 = np.append(visibility_b1, env_vis)
-    visibility_b2 = np.append(visibility_b2, env_vis)
+    # Always show both envelopes
+    visibility_b1 = np.append(visibility_b1, envelope_visibility)
+    visibility_b2 = np.append(visibility_b2, envelope_visibility)
 
-    if BPMs:
-        BPM_vis, BPM_traces = BPM_data(BPMs, plane)
+    # If BPM data was loaded from timber
+    if BPM_data:
+        BPM_visibility, BPM_traces = plot_BPM_data(BPM_data, plane)
         for i in BPM_traces:
             fig.add_trace(i, row=row, col=col)
 
-        visibility_b1 = np.append(visibility_b1, BPM_vis)
-        visibility_b2 = np.append(visibility_b2, BPM_vis)
+        # Always show BPM data for both beams
+        visibility_b1 = np.append(visibility_b1, BPM_visibility)
+        visibility_b2 = np.append(visibility_b2, BPM_visibility)
 
     return fig, visibility_b1, visibility_b2, row, col
 
-def add_knob_dropdown():
+def create_knob_dropdown():
 
+    # List of knobs that can be adjusted
     knobs = ['on_x1', 'on_x2h', 'on_x2v', 'on_x5', 'on_x8h', 'on_x8v',
              'on_sep1', 'on_sep2h', 'on_sep2v', 'on_sep5', 'on_sep8h', 'on_sep8v',
              'on_alice', 'on_lhcb']
     
-    # Dropdown widget
+    # Dropdown widget for knob selection
     dropdown_widget = widgets.Dropdown(
         options=knobs,
         description='Select knob:',
@@ -255,19 +304,18 @@ def add_knob_dropdown():
 def update_layout(fig, plane, row, col, visibility_b1, visibility_b2):
 
     # Set layout
-    fig.update_layout(height=600, width=800, showlegend=False)
+    fig.update_layout(height=global_height, width=global_width, showlegend=False, xaxis=dict(tickformat=','), yaxis=dict(tickformat=','), plot_bgcolor='white')
 
-    # Change the axes limits and labels
+    # Change x limits and labels
     fig.update_xaxes(title_text="s [m]", row=row, col=col)
 
+    # Change y limits and labels
     if plane == 'h': title = 'x [m]'
     elif plane == 'v': title = 'y [m]'
     fig.update_yaxes(title_text=title, range = [-0.05, 0.05], row=row, col=col)
-    
-    # Update the x-axis and y-axis tick format
-    fig.update_layout(xaxis=dict(tickformat=','), yaxis=dict(tickformat=','), plot_bgcolor='white')
 
-    if hasattr(aper_data, 'aper_b1') or hasattr(aper_data, 'colx_b1') or col_data:
+    # If aperture/collimators were loaded add buttons to switch between beam 1 and beam 2
+    if hasattr(global_data, 'aper_b1') or hasattr(global_data, 'colx_b1') or global_collimator_data:
 
         fig.update_layout(
         updatemenus=[
@@ -275,7 +323,7 @@ def update_layout(fig, plane, row, col, visibility_b1, visibility_b2):
                 type="buttons",
                 direction="right",
                 active=0,
-                x=0.62,
+                xanchor='left',
                 y=1.2,
                 buttons=list([
                     dict(label="Beam 1 aperture/collimation",
@@ -285,7 +333,7 @@ def update_layout(fig, plane, row, col, visibility_b1, visibility_b2):
                         method="update",
                         args=[{"visible": visibility_b2}])]))])
 
-def BPM_data(data, plane):
+def plot_BPM_data(data, plane):
     
     if plane == 'h': y_b1, y_b2 = data.b1_positions.X, data.b2_positions.X
     elif plane == 'v': y_b1, y_b2 = data.b1_positions.Y, data.b2_positions.Y
@@ -299,7 +347,7 @@ def BPM_data(data, plane):
     
     return np.full(2, True), [b1, b2]
 
-def machine_components(data):
+def plot_machine_components(data):
 
     # Specify the elements to plot and corresponding colors
     objects = ["SBEND", "COLLIMATOR", "SEXTUPOLE", "RBEND", "QUADRUPOLE"]
@@ -332,21 +380,21 @@ def machine_components(data):
         
     return visibility_arr, elements
 
-def collimators_from_yaml(data, plane):
+def plot_collimators_from_yaml(data, plane):
     
-    visibility_arr_b1, collimators = collimators(data, plane)
+    visibility_arr_b1, collimators = plot_collimators(data, plane)
     
     return visibility_arr_b1, collimators
 
-def collimators_from_timber(col_data, twiss_data, plane):
+def plot_collimators_from_timber(collimator_data, data, plane):
     
-    col_data.process(twiss_data) #TODO: make the names make sense!!!
+    collimator_data.process(data)
     
-    visibility_arr_b1, collimators_data = collimators(col_data, plane)
+    visibility_arr_b1, processed_collimator_data = plot_collimators(collimator_data, plane)
     
-    return visibility_arr_b1, collimators_data
+    return visibility_arr_b1, processed_collimator_data
 
-def collimators(data, plane):
+def plot_collimators(data, plane):
 
     if plane == 'h': df_b1, df_b2 = data.colx_b1, data.colx_b2
     elif plane == 'v': df_b1, df_b2 = data.coly_b1, data.coly_b2
@@ -373,7 +421,7 @@ def collimators(data, plane):
 
     return visibility_arr_b1, collimators
 
-def envelopes(data, plane):
+def plot_envelopes(data, plane):
 
     # Define hover template with customdata
     hover_template = ("s: %{x} [m]<br>"
@@ -424,7 +472,7 @@ def envelopes(data, plane):
 
     return visibility, traces
 
-def beam_positions(data, plane):
+def plot_beam_positions(data, plane):
 
     if plane == 'h': y_b1, y_b2 = data.tw_b1.x, data.tw_b2.x
     elif plane == 'v': y_b1, y_b2 = data.tw_b1.y, data.tw_b2.y
@@ -434,7 +482,7 @@ def beam_positions(data, plane):
     
     return np.full(2, True), [b1, b2]
 
-def nominal_beam_positions(data, plane):
+def plot_nominal_beam_positions(data, plane):
 
     if plane == 'h': y_b1, y_b2 = data.nom_b1.x, data.nom_b2.x
     elif plane == 'v': y_b1, y_b2 = data.nom_b1.y, data.nom_b2.y
@@ -444,7 +492,7 @@ def nominal_beam_positions(data, plane):
     
     return np.full(2, True), [b1, b2]
 
-def aperture(data, plane):
+def plot_aperture(data, plane):
 
     if plane == 'h':
             
@@ -467,16 +515,17 @@ def aperture(data, plane):
 
     return visibility, traces
 
-def add_velo(data, fig, row, col):
+def add_velo(data):
 
+    # Find ip8 position
     ip8 = data.tw_b1.loc[data.tw_b1['name'] == 'ip8', 's'].values[0]
 
-    fig.add_shape(type="rect",
-                x0=(ip8-0.2), x1=(ip8+0.8),
-                y0=-0.05, y1=0.05,
-                xref='x',
-                yref='y',
-                line=dict(
-                    color="Orange",
-                    width=1), row=row, col=col)
+    # VELO position
+    x0=ip8-0.2
+    x1=ip8+0.8
+    y0=-0.05
+    y1=0.05
 
+    trace = go.Scatter(x=[x0, x0, x1, x1], y=[y0, y1, y1, y0], fill="toself", mode='lines', line=dict(color='orange'), name='VELO')
+
+    return trace
--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -9,7 +9,7 @@ home_path = str(Path.cwd().parent)
 sys.path.append(home_path)
 
 from aper_package.aperture_data import Data
-from aper_package.plotting import create_figure
+from aper_package.plotting import *
 
 class TestPlotting(unittest.TestCase):
     
@@ -22,7 +22,7 @@ class TestPlotting(unittest.TestCase):
 
     def test_create_figure(self):
         
-        fig, visibility_b1, visibility_b2, row, col = create_figure(self.data, 'h', None, None)
+        fig, visibility_b1, visibility_b2, row, col = create_figure(self.data, 'h', None, None, None)
         
         expected_visibility_b1 = np.array([ True,  True, False, False,  True,  True,  True,  True,  True, True,  True,  True])
         expected_visibility_b2 = np.array([False, False,  True,  True,  True,  True,  True,  True,  True, True,  True,  True])
@@ -38,6 +38,140 @@ class TestPlotting(unittest.TestCase):
         self.assertEqual(len(fig.data[4]['x']), 34939)
         self.assertEqual(fig.data[5]['text'][0], 'lhcb2ip1.l1_p_')
         self.assertEqual(fig.data[6]['text'][0], 'ip1')
+
+        fig, visibility_b1, visibility_b2, row, col = create_figure(self.data, 'v', None, None, None)
+
+        expected_visibility_b1 = np.array([True, True, False, False, True, True, True, True, True, True, True, True])
+        expected_visibility_b2 = np.array([False, False, True, True, True, True, True, True, True, True, True, True])
+        np.testing.assert_array_equal(visibility_b1, expected_visibility_b1)
+        np.testing.assert_array_equal(visibility_b2, expected_visibility_b2)
+        self.assertEqual(row, 1)
+        self.assertEqual(col, 1)
+        self.assertEqual(len(fig.data), 12)
+
+        self.assertEqual(len(fig.data[1]['y']), 29057)
+        self.assertEqual(len(fig.data[2]['x']), 29085)
+        self.assertEqual(len(fig.data[3]['y']), 29085)
+        self.assertEqual(len(fig.data[4]['x']), 34939)
+        self.assertEqual(fig.data[5]['text'][0], 'lhcb2ip1.l1_p_')
+        self.assertEqual(fig.data[6]['text'][0], 'ip1')
+
+        self.data.load_elements(path=home_path+'/test_data/thick_all_optics_B1.tfs')
+
+        fig, visibility_b1, visibility_b2, row, col = create_figure(self.data, 'v', None, None, None)
+        self.assertEqual(row, 2)
+        self.assertEqual(col, 1)
+        self.assertEqual(len(fig.data), 3837)
+        self.assertEqual(len(visibility_b1), 3837)
+        self.assertEqual(len(visibility_b2), 3837)
+
+        # Assuming fig.data[1000]['x'] exists and is comparable
+        expected_values = (21642.38092286263, 21642.38092286263, 21656.680922862633, 21656.680922862633)
+        
+        for actual, expected in zip(fig.data[1000]['x'], expected_values):
+            self.assertAlmostEqual(actual, expected, places=10)
+
+    def test_plot_envelopes(self):
+
+        vis, traces = plot_envelopes(self.data, 'h')
+
+        np.testing.assert_array_equal(vis, np.array([True, True, True, True]))
+        self.assertEqual(len(traces), 4)
+
+        self.assertAlmostEqual(traces[0]['x'][0], 0, 10)
+        self.assertAlmostEqual(traces[0]['x'][-1], 26658.88317687314, 10)
+        self.assertAlmostEqual(traces[0]['y'][0], -0.002000142862473192, 10)
+        self.assertAlmostEqual(traces[0]['y'][-1], -0.0020001428624731817, 10)
+
+        self.assertEqual(len(traces[0]['x']), self.data.tw_b1.shape[0])
+        self.assertEqual(len(traces[1]['x']), self.data.tw_b1.shape[0])
+        self.assertEqual(len(traces[2]['y']), self.data.tw_b2.shape[0])
+        self.assertEqual(len(traces[3]['y']), self.data.tw_b2.shape[0])
+
+        vis, traces = plot_envelopes(self.data, 'v')
+
+        np.testing.assert_array_equal(vis, np.array([True, True, True, True]))
+        self.assertEqual(len(traces), 4)
+
+        self.assertAlmostEqual(traces[0]['y'][0], -2.1961356293530347e-09, 10)
+        self.assertAlmostEqual(traces[0]['y'][-1], -2.1961356297416947e-09, 10)
+
+        self.assertEqual(len(traces[0]['x']), self.data.tw_b1.shape[0])
+        self.assertEqual(len(traces[1]['x']), self.data.tw_b1.shape[0])
+        self.assertEqual(len(traces[2]['y']), self.data.tw_b2.shape[0])
+        self.assertEqual(len(traces[3]['y']), self.data.tw_b2.shape[0])
+
+    def test_plot_beam_positions(self):
+
+        vis, traces = plot_beam_positions(self.data, 'h')
+
+        np.testing.assert_array_equal(vis, np.array([True, True]))
+        self.assertEqual(len(traces), 2)
+
+        self.assertAlmostEqual(traces[0]['x'][1000], 261.1706666666666, 10)
+        self.assertAlmostEqual(traces[1]['x'][1000], 264.08433171133584, 10)
+        self.assertAlmostEqual(traces[0]['y'][1000], 9.474557711656808e-07, 10)
+        self.assertAlmostEqual(traces[1]['y'][1000], -1.2460130805535723e-07, 10)
+
+        nom_vis, nom_traces = plot_beam_positions(self.data, 'h')
+
+        self.assertTrue((nom_traces[0]['x']==traces[0]['x']).all())
+        self.assertTrue((nom_traces[1]['x']==traces[1]['x']).all())
+        self.assertTrue((nom_traces[0]['y']==traces[0]['y']).all())
+        self.assertTrue((nom_traces[1]['y']==traces[1]['y']).all())
+
+        vis, traces = plot_beam_positions(self.data, 'v')
+
+        np.testing.assert_array_equal(vis, np.array([True, True]))
+        self.assertEqual(len(traces), 2)
+
+        self.assertAlmostEqual(traces[0]['y'][2000], -2.5213925117160937e-07, 10)
+        self.assertAlmostEqual(traces[1]['y'][2000], 1.3734680420729657e-07, 10)
+
+        nom_vis, nom_traces = plot_beam_positions(self.data, 'v')
+
+        self.assertTrue((nom_traces[0]['x']==traces[0]['x']).all())
+        self.assertTrue((nom_traces[1]['x']==traces[1]['x']).all())
+        self.assertTrue((nom_traces[0]['y']==traces[0]['y']).all())
+        self.assertTrue((nom_traces[1]['y']==traces[1]['y']).all())
+
+    def test_plot_aperture(self):
+
+        vis, traces = plot_aperture(self.data, 'v')
+
+        np.testing.assert_array_equal(vis, np.array([True, True, False, False]))
+        self.assertEqual(len(traces), 4)
+
+        self.assertEqual(traces[0]['text'][50], 'MQXA.1R1..45')
+        self.assertEqual(traces[1]['text'][50], 'MQXA.1R1..45')
+        self.assertEqual(traces[2]['text'][50], 'MQXA.1R1..84')
+        self.assertEqual(traces[3]['text'][50], 'MQXA.1R1..84')
+
+        self.assertAlmostEqual(traces[0]['y'][100], 0.02385, 10)
+        self.assertAlmostEqual(traces[2]['y'][100], 0.02385, 10)
+
+        self.assertTrue((traces[0]['y']==-traces[1]['y']).all())
+        self.assertTrue((traces[2]['y']==-traces[3]['y']).all())
+        self.assertTrue((traces[0]['x']==traces[1]['x']).all())
+        self.assertTrue((traces[2]['x']==traces[3]['x']).all())
+
+        vis, traces = plot_aperture(self.data, 'h')
+
+        np.testing.assert_array_equal(vis, np.array([True, True, False, False]))
+        self.assertEqual(len(traces), 4)
+
+        self.assertEqual(traces[0]['text'][50], 'MQXA.1R1..45')
+        self.assertEqual(traces[1]['text'][50], 'MQXA.1R1..45')
+        self.assertEqual(traces[2]['text'][50], 'MQXA.1R1..84')
+        self.assertEqual(traces[3]['text'][50], 'MQXA.1R1..84')
+
+        self.assertAlmostEqual(traces[0]['y'][100], 0.01895, 10)
+        self.assertAlmostEqual(traces[2]['y'][100], 0.01895, 10)
+
+        self.assertTrue((traces[0]['y']==-traces[1]['y']).all())
+        self.assertTrue((traces[2]['y']==-traces[3]['y']).all())
+        self.assertTrue((traces[0]['x']==traces[1]['x']).all())
+        self.assertTrue((traces[2]['x']==traces[3]['x']).all())
 
 
 if __name__ == '__main__':
